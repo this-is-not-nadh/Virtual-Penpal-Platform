48056627c2e0b49929076b75232f30f2e45dbe1a
Nadhrahtul Iman <nutinnutshell15@gmail.com>
2025-08-22 23:00:29 +0800

    Reverted changes

--- src/App.jsx
+++ src/App.jsx
@@ -82,7 +82,7 @@ function LoginForm({ onLoginSuccess }) {
 
   // Hardcoded credentials (placeholder for now; might be permanent if I'm lazy)
   const validCredentials = [
-    { username: 'Jono', password: 'BonerBoner', name: 'Jono' },
+    { username: 'Q38', password: 'Sunshine', name: 'Nate' },
     { username: 'Q09', password: 'Moonlight', name: 'Nadh'}
   ];
 
